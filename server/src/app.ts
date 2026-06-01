788a84ed1de3a480ead36ce7813e0dea18bfeb5e
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-06-01 16:30:44 +0000

    Allow cors on backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import router from "./routes/index.js";
 
@@ -12,11 +13,12 @@ import "./config/passport.js";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use(express.json());
 
 app.use(router);
 
