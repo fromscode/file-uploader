807372f287de18a938e7def6c485dfe51f8a7623
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-09-12 10:35:24 +0000

    Rename DriveCloud -> Duke's Archives

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="bg-blue-800 text-2xl text-blue-50 px-2 py-3">
       <NavLink to="/" className="cursor-pointer">
-        DriveCloud
+        Duke's Archives
       </NavLink>
     </nav>
   );
