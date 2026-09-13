82a737844f290d859a6fb606d313c7b3f405dd8c
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-09-13 05:14:06 +0000

    Small css fix

--- client/src/Error.tsx
+++ client/src/Error.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 export default function Error() {
   return (
-    <div className="h-screen items-center justify-center flex flex-col pb-48 gap-5">
+    <div className="h-screen items-center justify-center flex flex-col pb-48 gap-5 text-zinc-300">
       <div className="text-2xl flex justify-center items-center">
         404 | The requested page could not be found
       </div>
